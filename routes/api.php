9813adf388b288a37661956e76c99aa971bac06f
Andrea Mapelli <info@ns7records.com>
2018-06-04 15:02:43 +0200

    New API for Edit & Delete event

--- routes/api.php
+++ routes/api.php
@@ -13,19 +13,30 @@ use Illuminate\Http\Request;
 |
 */
 
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+
+// Route::post('signup','Userscontroller@signup');
+// Route::post('login','Userscontroller@login');
+// Route::post('forget','Userscontroller@forget');
+// Route::get('password/reset/{token}', 'Userscontroller@getReset');
+// Route::post('createevent', 'ApiController@createevent');
+
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::post('signup','Api\ApiController@signup');
+Route::post('login','Api\ApiController@login');
+Route::post('forget','Api\ApiController@forget');
+Route::get('password/reset/{token}', 'Api\ApiController@getReset');
 
-// Route::group(['namespace'=>'api'],function(){
+Route::post('createevent', 'Api\ApiController@createevent');
+Route::post('deleteevent', 'Api\ApiController@deleteevent');
+Route::post('editevent', 'Api\ApiController@editevent');
+Route::post('joinevent', 'Api\ApiController@joinevent');
+Route::post('disjoinevent', 'Api\ApiController@disjoinevent');
 
-	Route::post('signup','Userscontroller@signup');
-	Route::post('login','Userscontroller@login');
-	Route::post('forget','Userscontroller@forget');
-	Route::get('password/reset/{token}', 'Userscontroller@getReset');
-
-	
-
-
-// });
